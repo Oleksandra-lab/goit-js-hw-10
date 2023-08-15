1c53572d75e9ad20693ba559faa55f89706ed031
Oleksandra-lab <tka4olex@gmail.com>
2023-08-15 10:58:52 +0300

    cat-api-start

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_GY9nrOlvgJyWyzy2LZQbzeyoqBx9MUWKZ7QHgGLvrMlghHtvbTN8XOeaUza3OUtD";
+import {fetchBreeds} from "./js/cat-api";
---
+++ src/js/cat-api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+const API_KEY = 'live_GY9nrOlvgJyWyzy2LZQbzeyoqBx9MUWKZ7QHgGLvrMlghHtvbTN8XOeaUza3OUtD';
+
+axios.defaults.headers.common["x-api-key"] = API_KEY;
+
+const BREED_URL = 'https://api.thecatapi.com/v1/breeds'
+const SEARCH_URL = 'https://api.thecatapi.com/v1/images/search'
+
+export function fetchBreeds() {
+    return axios.get(`${BREED_URL}`)
+    .then((resp) => {
+        if(!resp.ok) {
+            throw new Error(resp.statusText)
+        }
+        return resp.data
+    } )
+
+}
+console.log(fetchBreeds());
+
+export function fetchCatByBreed(breedId) {
+    return axios.get(`${SEARCH_URL}?breed_ids=${breedId}`)
+    .then((resp) => {
+        if(!resp.ok) {
+            throw new Error(resp.statusText)
+        }
+        return resp.data
+    } )
+
+}
+console.log(fetchCatByBreed(breedId));
