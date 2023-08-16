0ca6261b4dde567e707fe7cff622c5960a87bc39
Oleksandra-lab <tka4olex@gmail.com>
2023-08-16 20:35:43 +0300

    cat-api finished

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import {fetchBreeds, fetchCatByBreed } from "./js/cat-api";
+import './styles.css'
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const selector = document.querySelector('.breed-select');
-const catCard = document.querySelector('.cat-info');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
+const selectorEl = document.querySelector('.breed-select');
+const catCardEl = document.querySelector('.cat-info');
+const loaderEl = document.querySelector('.loader');
+const errorEl = document.querySelector('.error');
+
+loaderEl.classList.replace('loader', 'is-hidden');
+errorEl.classList.add('is-hidden');
+catCardEl.classList.add('is-hidden');
 
 let arrBreedsId = [];
 fetchBreeds()
@@ -15,18 +20,42 @@ fetchBreeds()
         arrBreedsId.push({text: element.name, value: element.id});
     });
     new SlimSelect({
-        select: selector,
+        select: selectorEl,
         data: arrBreedsId
     });
     })
-.catch(FetchError);
+.catch(fetchError);
 
 
-selector.addEventListener('change', onSelectBreed)
+selectorEl.addEventListener('change', onSelectBreed)
 
-function FetchError(error) {
-    selector.classList.remove('is-hidden');
-    loader.classList.replace('loader', 'is-hidden');
+function onSelectBreed(e) {
+    loaderEl.classList.replace('is-hidden', 'loader');
+    selectorEl.classList.add('is-hidden');
+    catCardEl.classList.add('is-hidden');
+
+    const breedId = e.currentTarget.value;
+    fetchCatByBreed(breedId).then(data => {
+        loaderEl.classList.replace('loader', 'is-hidden');
+      selectorEl.classList.remove('is-hidden');
+
+        const { url, breeds } = data[0];
+
+        catCardEl.innerHTML = `<div class="card-img">
+        <img src="${url}" alt="${breeds[0].name}" width="300"/>
+        </div>
+        <div class="card">
+        <h1>${breeds[0].name}</h1>
+        <p>${breeds[0].description}</p>
+        <p><b>Temperament:</b> ${breeds[0].temperament}</p>
+        </div>`
+        catCardEl.classList.remove('is-hidden');
+    }).catch(fetchError);
+};
+
+function fetchError(error) {
+    selectorEl.classList.remove('is-hidden');
+    loaderEl.classList.replace('loader', 'is-hidden');
 
     Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
         position: 'center-center',
